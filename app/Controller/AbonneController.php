85436f65b0ed68af3ddc98a1bf2b3dbefc78a6c2
BouazziH <bouazzicdi@gmail.com>
2022-09-22 14:25:22 +0200

    Ajout d'un controlleur

--- app/Controller/AbonneController.php
+++ app/Controller/AbonneController.php
@@ -3,20 +3,44 @@
 namespace App\Controller;
 
 use App\Weblitzer\Controller;
+use App\Model\AbonneModel;
 
-use App\mode
-/**
- *
- */
-class AbonneeController extends Controller
-{
 
+
+class AbonneController extends Controller
+{   
     public function listing()
     {
        
-        
+        $abonnes = AbonneModel::all();
+        $count  = AbonneModel::count();
+        $this->render('app.abonne.listing',
+        ['abonnes'=>$abonnes
+        ,'count'=>$count]);
     }
-
+    public function show($id)
+    {
+        $abonne = $this->isAbonneExistor404($id);
+        $this->render('app.abonne.show',
+        ['abonne' => $abonne
+    ]);
+    }
     
+ private function isAbonneExistor404($id)
+ {
+    $abonne = AbonneModel::findById($id);
+    if(empty($abonne))
+    {
+           $this->Abort404();
+    }
+ return $abonne;
+ }
 
+ public function delete($id)
+ {
+    $abonne = $this->isAbonneExistor404($id);
+    AbonneModel::delete($id);
+    $this->redirect(('liste'));
+    
+ }
 }
