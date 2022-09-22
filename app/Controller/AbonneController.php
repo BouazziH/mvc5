14e8076995c2c29920a3c932549a5ea3267e0218
BouazziH <bouazzicdi@gmail.com>
2022-09-22 11:40:48 +0200

    Creation d'un new controller pour lister les abonnees

---
+++ app/Controller/AbonneController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Weblitzer\Controller;
+
+use App\mode
+/**
+ *
+ */
+class AbonneeController extends Controller
+{
+
+    public function listing()
+    {
+       
+        
+    }
+
+    
+
+}
