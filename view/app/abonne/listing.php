0f246300dc84687f80db4635cd96a310a5c2088c
BouazziH <bouazzicdi@gmail.com>
2022-09-22 14:27:42 +0200

    Adding repertoire abonne apres page.php selon la methode

---
+++ view/app/abonne/listing.php
@@ -0,0 +1,32 @@
+<h2>liste des abonnées</h2>
+<p>Nombre d'abonnées :
+    <?= $count ?>
+</p>
+<section class="abonne">
+    <table>
+        <?php foreach ($abonnes as $abonne) { ?>
+        <thead>
+            <th>nom</th>
+            <th>prenom</th>
+            <th>age</th>
+            <th>Afficher</th>
+            <th>Editer</th>
+            <th>Supprimer</th>
+        </thead>
+        <tbody>
+            <td> <?= $abonne->nom; ?></td>
+            <td><?= $abonne->prenom; ?></td>
+            <td><?= $abonne->age; ?></td>
+            <td> <a href="<?= $view->path('detail', [$abonne->id]); ?>">show</a></td>
+            <td> <a href="<?= $view->path('edit', [$abonne->id]); ?>">Edit</a></td>
+            <td>
+                <a onClick="return confirm ('do you realy want to delete this persone')"
+                    href="<?= $view->path('delete', [$abonne->id]); ?>"> Delete</a>
+            </td>
+        </tbody>
+
+
+        <?php
+        } ?>
+    </table>
+</section>
---
+++ view/app/abonne/show.php
@@ -0,0 +1,9 @@
+<div class="Unabonne">
+    <p><?=$abonne->nom;?>
+    </p>
+    <p>
+        <?=$abonne->prenom;?>
+    </p>
+    <p> <?=$abonne->email;?></p>
+    <p><?=$abonne->age;?> ans</p>
+</div>
